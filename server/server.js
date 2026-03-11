85b27e3a862011718a50640e7d5d50ca3379765f
abhilashwork17-debug <abhilash.work.17@gmail.com>
2026-03-12 00:37:21 +0530

    db.js file created for the connection of the mongodb with the nodejs and also updated the server.js file for connecting it with db.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const cors = require('cors')
 const app = express();
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
 
 const authRoutes = require('./routes/authRoutes')
 const aiRoutes = require('./routes/aiRoutes')
 const PORT = process.env.PORT || 3000;
 
 
+// ENV
+require('dotenv').config();
+connectDB();
 
 app.use('/api/auth',authRoutes)
 app.use('/api/ai',aiRoutes)
