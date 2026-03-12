d81215e5be118da2652ee5498ff3452a673355f9
abhilashwork17-debug <abhilash.work.17@gmail.com>
2026-03-12 16:58:30 +0530

    Major changes has been made in the db.js as well as many error has been fixed

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const authController = require('../controllers/authController')
 
 // Register a new user
-router.post('/register',authController.register);
+router.post('/register', authController.registerUser);
 
 // Login a user
-router.post('/login',authController.login);
+// router.post('/login',authController.login);
 
 // Verify OTP
-router.post('/verify-otp',authController.verifyOTP);
+// router.post('/verify-otp',authController.verifyOTP);
 
-modeule.exports = router;
+module.exports = router;
