581e9f3abff6cac7db2e5adc75ea424caf9226a8
abhilashwork17-debug <abhilash.work.17@gmail.com>
2026-03-12 17:32:22 +0530

    debugging in authController.js and addition of middleware in server,js and also removal of next() in the user.js

--- server/models/User.js
+++ server/models/User.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
   },
   username: {
     type: String,
-    require: true
+    required: true
   },
   isVerified: {
     type: Boolean,
@@ -30,19 +30,18 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save',async function (next) {
-  if(!this.isModified('password')){
-    return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) {
+    return;
   }
   const salt = await bcrypt.genSalt(12);
-  this.password = await bcrypt.hash(this.password,salt);
-  next();
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Compare password before login
 userSchema.methods.comparePassword = async function (candidatePassword) {
-  return await bcrypt.compare(candidatePassword,this.password);
+  return await bcrypt.compare(candidatePassword, this.password);
 }
 
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema);
 module.exports = User;
