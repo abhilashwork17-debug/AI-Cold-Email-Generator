a87cfd7b6ef229991806ae3394445c5f093fd9d7
abhilashwork17-debug <abhilash.work.17@gmail.com>
2026-03-28 23:50:40 +0530

    Addition of aiController

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router();
 const aiController = require('../controllers/aiController');
+const protect = require('../middleware/authMiddleware')
 
-router.post('/generate-email', aiController.generateEmail);
+router.post('/generate-email', protect, aiController.generateEmail);
 
 module.exports = router;
