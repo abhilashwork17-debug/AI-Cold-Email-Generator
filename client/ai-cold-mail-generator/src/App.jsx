285d8325f0e670aae7cb500e8a87ae5f85f992b4
abhilashwork17-debug <abhilash.work.17@gmail.com>
2026-04-27 19:48:51 +0530

    setting up the front end part and adding all the pages..........here after the exam break

--- client/ai-cold-mail-generator/src/App.jsx
+++ client/ai-cold-mail-generator/src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
-
+import React, { useState } from "react";
+import { useAuth } from './context/authContext'
+import { Router } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
+  const { user,loading } = useAuth();
+
+  if(loading){
+    return <p>Loading...</p>;
+  }
 
   return (
-    <>
-    <p>Hello World</p>
-    </>
-  )
+    <Router>
+      <Toaster position = "top-right"/>
+      <Routes>
+        <Routes path = "/" element = {<LandingPage/>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- client/ai-cold-mail-generator/tailwind.config.js
+++ client/ai-cold-mail-generator/tailwind.config.js
@@ -1,33 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          50:  "#EEF2FF",
-          100: "#E0E7FF",
-          200: "#C7D2FE",
-          300: "#A5B4FC",
-          400: "#818CF8",
-          500: "#6366F1", 
-          600: "#4F46E5",
-          700: "#4338CA",
-          800: "#3730A3",
-          900: "#312E81",
-        },
-        secondary: "#06B6D4",
-        accent: "#F59E0B",
-        dark: "#0F172A",
-        light: "#F8FAFC",
-      },
-      fontFamily: {
-        sans: ["Inter", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
