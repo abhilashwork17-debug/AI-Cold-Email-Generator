f464951e03ae3a44ff3f9a7025b897df77c9bca8
abhilashwork17-debug <abhilash.work.17@gmail.com>
2026-05-10 16:03:40 +0530

    removing unecessary code segments

--- server/server.js
+++ server/server.js
@@ -6,30 +6,30 @@ const app = express();
 
 const connectDB = require('./config/db');
 
-// 👉 Routes AFTER dotenv
+
 const atsRoutes = require("./routes/atsRoutes");
 const authRoutes = require('./routes/authRoutes');
 const aiRoutes = require('./routes/aiRoutes');
-const resumeWriterRoutes = require("./routes/resumeWriterRoutes");
+
 
 const PORT = process.env.PORT || 3000;
 
-// MIDDLEWARE
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Connect to MongoDB
+
 connectDB();
 
-// ROUTES
+
 app.use('/api/auth', authRoutes);
 app.use('/api/ai', aiRoutes);
 app.use("/api/ats", atsRoutes);
 app.use("/api/resume", resumeWriterRoutes);
 
 
-// Error handler
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
