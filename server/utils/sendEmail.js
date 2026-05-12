a8f58f57e495fc4a192778ac5119592808ba2e1a
abhilashwork17-debug <abhilash.work.17@gmail.com>
2026-05-13 00:14:51 +0530

    updating the mail for the mailsmith own mail

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -15,7 +15,7 @@ const sendEmail = async (options) => {
     });
 
     const mailOptions = {
-      from: process.env.EMAIL_USER,
+      from: `"MailSmith Security" <${process.env.EMAIL_USER}>`,
       to: options.email,
       subject: options.subject,
       text: options.message,
